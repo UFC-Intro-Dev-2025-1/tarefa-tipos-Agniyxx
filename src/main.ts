e2943e3b34e156599bf5e51c7c25aaec77a20cb9
Agne Stephane <agne.steph.moss@gmail.com>
2025-05-23 01:28:05 +0000

    feat: solução d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 // Escreva seu código aqui!
+const numeroDeJanelas: number = 18
+
+let monitor: string = 'Victor'
+
+let luzEstaLigada: boolean = true
+
+let quantidadeDeComputadores
+
+let aluno = {
+    nome: 'Agne',
+    idade: '22',
+}
+
+console.log(numeroDeJanelas)
+console.log(monitor)
+console.log(luzEstaLigada)
+console.log(quantidadeDeComputadores)
+console.log(aluno.nome)
+console.log(aluno.idade)
+
